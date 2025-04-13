d41cbfb85f7a19a01411dd3df35d6b193f8925f6
Penguin <amasharafutdinov@kpfu.ru>
2025-04-13 14:02:14 +0300

    Added TgUserModel

---
+++ src/backend/Core/Models/TgUserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Models;
+
+public class TgUserModel
+{
+    public required long TgId { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required string Patronymic { get; set; }
+    public required string PhoneNumber { get; set; }
+}
