c8335c79131e500e368bfbb80824d6c3def78feb
Yuizhey <Gre4ka28@yandex.ru>
2025-04-14 17:30:36 +0300

    расширил сервес

--- src/backend/Application/Services/EnrollmentService.cs
+++ src/backend/Application/Services/EnrollmentService.cs
@@ -25,4 +25,59 @@ public class EnrollmentService : IEnrollmentService
         var courseTitles = coursesResult.Data.Select(c => c.Title).ToList();
         return Result<ICollection<string>>.Success(courseTitles);
     }
+    
+    public async Task<Result<Enrollment>> AddAsync(Enrollment enrollment)
+    {
+        var result = await repository.AddAsync(enrollment);
+        if (!result.IsSuccess)
+        {
+            return Result<Enrollment>.Failure(result.ErrorMessage);
+        }
+
+        return Result<Enrollment>.Success(result.Data);
+    }
+
+    public async Task<Result<Enrollment>> UpdateAsync(Enrollment enrollment)
+    {
+        var result = await repository.UpdateAsync(enrollment);
+        if (!result.IsSuccess)
+        {
+            return Result<Enrollment>.Failure(result.ErrorMessage);
+        }
+
+        return Result<Enrollment>.Success(result.Data);
+    }
+
+    public async Task<Result> DeleteAsync(Enrollment enrollment)
+    {
+        var result = await repository.DeleteAsync(enrollment);
+        if (!result.IsSuccess)
+        {
+            return Result.Failure(result.ErrorMessage);
+        }
+
+        return Result.Success();
+    }
+
+    public async Task<Result<Enrollment?>> GetByIdAsync(int id)
+    {
+        var result = await repository.GetByIdAsync(id);
+        if (!result.IsSuccess)
+        {
+            return Result<Enrollment?>.Failure(result.ErrorMessage);
+        }
+
+        return Result<Enrollment?>.Success(result.Data);
+    }
+
+    public async Task<Result<ICollection<Enrollment>>> GetAllAsync()
+    {
+        var result = await repository.GetAllAsync();
+        if (!result.IsSuccess)
+        {
+            return Result<ICollection<Enrollment>>.Failure(result.ErrorMessage);
+        }
+
+        return Result<ICollection<Enrollment>>.Success(result.Data);
+    }
 }
--- src/backend/Core/Interfaces/Services/IEnrollmentService.cs
+++ src/backend/Core/Interfaces/Services/IEnrollmentService.cs
@@ -1,8 +1,17 @@
+using Core.Models;
 using Core.Utils;
 
 namespace Core.Interfaces.Services;
 
 public interface IEnrollmentService
 {
-    Task<Result<ICollection<string>>> GetCourseTitlesByUserId(int id);
+    public interface IEnrollmentService
+    {
+        Task<Result<ICollection<string>>> GetCourseTitlesByUserId(int userId);
+        Task<Result<Enrollment>> AddAsync(Enrollment enrollment);
+        Task<Result<Enrollment>> UpdateAsync(Enrollment enrollment);
+        Task<Result> DeleteAsync(Enrollment enrollment);
+        Task<Result<Enrollment?>> GetByIdAsync(int id);
+        Task<Result<ICollection<Enrollment>>> GetAllAsync();
+    }
 }
