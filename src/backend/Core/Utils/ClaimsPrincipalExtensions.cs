f2cad6e61c864e17f78daad262e41cd658888846
Penguin <amasharafutdinov@kpfu.ru>
2025-04-24 21:25:47 +0300

    добавить ClaimsPrincipalExtensions для извлечения userId

---
+++ src/backend/Core/Utils/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Core.Models;
+
+namespace Core.Utils;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static bool TryGetUserId(this ClaimsPrincipal? user, out int userId)
+    {
+        userId = default;
+
+        if (user is null)
+            return false;
+
+        var claim = user.FindFirst(ClaimTypes.NameIdentifier);
+        if (claim is null)
+            return false;
+
+        return int.TryParse(claim.Value, out userId);
+    }
+}
