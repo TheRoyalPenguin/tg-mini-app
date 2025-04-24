3cdbb834fc8a307db7edfb132ea3e02b86631b28
Penguin <amasharafutdinov@kpfu.ru>
2025-04-24 19:54:21 +0300

    Почистил API/Controllers/ModuleController.cs

--- src/backend/API/Controllers/ModuleController.cs
+++ src/backend/API/Controllers/ModuleController.cs
@@ -1,8 +1,5 @@
-using API.DTO;
-using API.DTO.ModuleRequests;
 using AutoMapper;
 using Core.Interfaces.Services;
-using Core.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -13,47 +10,6 @@ public class ModuleController(
     IModuleService moduleService,
     IMapper mapper) : ControllerBase
 {
-    [HttpGet("all")]
-    public async Task<IActionResult> GetAllAsync()
-    {
-        var serviceResult = await moduleService.GetAllModulesAsync();
-
-        return serviceResult.IsSuccess 
-            ? Ok(serviceResult.Data)
-            : Problem(serviceResult.ErrorMessage);
-    }
-    
-    [HttpPost]
-    public async Task<IActionResult> CreateAsync([FromBody] NewModuleRequest request)
-    {
-        var model = mapper.Map<Module>(request);
-
-        var serviceResult = await moduleService.AddModuleAsync(model);
-        return serviceResult.IsSuccess 
-            ? Ok(serviceResult.Data) 
-            : Problem(serviceResult.ErrorMessage);
-    }
-    
-    [HttpPut]
-    public async Task<IActionResult> UpdateAsync([FromBody] UpdateModuleRequest request)
-    {
-        var model = mapper.Map<Module>(request);
-
-        var serviceResult = await moduleService.UpdateModuleAsync(model);
-        return serviceResult.IsSuccess 
-            ? Ok(serviceResult.Data) 
-            : Problem(serviceResult.ErrorMessage);
-    }
-    
-    [HttpDelete("{moduleId:int}")]
-    public async Task<IActionResult> DeleteAsync(int moduleId)
-    {
-        var serviceResult = await moduleService.DeleteModuleAsync(moduleId);
-        
-        return serviceResult.IsSuccess 
-            ? Ok() 
-            : Problem(serviceResult.ErrorMessage);
-    }
     
     [HttpGet("{moduleId:int}")]
     public async Task<IActionResult> GetModuleByIdAsync(int moduleId)
