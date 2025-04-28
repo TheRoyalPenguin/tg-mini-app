191a5236cb00b3943541bc4cca9ddb11f7cd7d23
Penguin <42300062+TheRoyalPenguin@users.noreply.github.com>
2025-04-29 01:37:08 +0300

    Реализация NotificationsController  и BotHostedService

---
+++ src/tg-bot/Bot/BotHostedService.cs
@@ -0,0 +1,31 @@
+﻿using Bot.Handlers;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+
+namespace Bot;
+
+public class BotHostedService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ITelegramBotClient _botClient;
+
+    public BotHostedService(ITelegramBotClient botClient, IServiceProvider serviceProvider)
+    {
+        _botClient = botClient;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var scope = _serviceProvider.CreateScope();
+        var updateHandler = scope.ServiceProvider.GetRequiredService<UpdateHandler>();
+
+        _botClient.StartReceiving(
+            updateHandler.HandleUpdateAsync,
+            updateHandler.HandleErrorAsync,
+            new ReceiverOptions(),
+            cancellationToken: stoppingToken
+        );
+        return Task.CompletedTask;
+    }
+}
---
+++ src/tg-bot/Bot/Controllers/NotificationsController.cs
@@ -0,0 +1,36 @@
+﻿using Bot.Interfaces;
+using Bot.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bot.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationsController : ControllerBase
+{
+    private readonly ITelegramService _telegramService;
+
+    public NotificationsController(ITelegramService telegramService)
+    {
+        _telegramService = telegramService;
+    }
+
+    public class SendRequest
+    {
+        public long ChatId { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+
+    [HttpPost("send")]
+    public async Task<IActionResult> Send([FromBody] SendRequest request)
+    {
+        if (request.ChatId == 0 || string.IsNullOrWhiteSpace(request.Message))
+            return BadRequest("Invalid request");
+
+        var text = request.Message;
+
+        await _telegramService.SafeSendMessageAsync(request.ChatId, text, new CancellationToken());
+
+        return Ok();
+    }
+}
